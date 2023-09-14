b20095eb77db992ef822471374dd08eb6c57424a
JUNAID-TARIQ1991 <JUNAID-TARIQ1991>
2023-09-14 12:16:22 +0500

    Modified

--- Simple_python_problems/hacker_rank_problems/prob11_prac.py
+++ Simple_python_problems/hacker_rank_problems/prob11_prac.py
@@ -26,3 +26,65 @@ for row in row:
 
 arr = [[i for i in range(4)] for j in range(3)]
 print(arr)
+
+
+# Python 3 program to find the maximum
+# sum of hour glass in a Matrix
+
+# Fixing the size of the Matrix.
+# Here it is of order 6 x 6
+
+R = 5
+C = 5
+
+# Function to find the maximum sum of hour glass
+
+
+def MaxSum(arr):
+
+    # Considering the matrix also contains
+    max_sum = -50000
+
+    # Negative values , so initialized with
+    # -50000. It can be any value but very
+    # smaller.
+    # max_sum=0 -> Initialize with 0 only if your
+    # matrix elements are positive
+
+    if (R < 3 or C < 3):
+        print("Not possible")
+        exit()
+
+    # Here loop runs (R-2)*(C-2) times considering
+    # different top left cells of hour glasses.
+    for i in range(0, R-2):
+        for j in range(0, C-2):
+
+            # Considering arr[i][j] as top
+            # left cell of hour glass.
+            SUM = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + (arr[i + 1][j + 1]) + (arr[i + 2][j] +
+                                                                                       arr[i + 2][j + 1] + arr[i + 2][j + 2])
+
+            # If previous sum is less
+            # then current sum then
+            # update new sum in max_sum
+            if (SUM > max_sum):
+                max_sum = SUM
+            else:
+                continue
+
+    return max_sum
+
+
+# Driver Code
+arr = [[1, 2, 3, 0, 0],
+       [0, 0, 0, 0, 0],
+       [2, 1, 4, 0, 0],
+       [0, 0, 0, 0, 0],
+       [1, 1, 0, 1, 0]]
+res = MaxSum(arr)
+
+print(f"Maximum sum of hour glass = {res}")
+
+# This code is written by Akshay Prakash
+# Code is modified by Susobhan Akhuli
